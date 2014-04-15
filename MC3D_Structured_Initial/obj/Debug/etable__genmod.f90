6d996fbbb8639cc43d13bf92dc31ee6104210cfb
PY Chuang <ives0228@gmail.com>
2014-04-16 01:22:24 +0800

    Modified Initial Program @ 04/15/2014

---
+++ MC3D_Structured_Initial/obj/Debug/etable__genmod.f90
@@ -0,0 +1,11 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Apr 16 01:14:43 2014
+        MODULE ETABLE__genmod
+          INTERFACE 
+            SUBROUTINE ETABLE(MAT,IDX,E,OUT)
+              INTEGER(KIND=4) :: MAT
+              INTEGER(KIND=4) :: IDX
+              REAL(KIND=8) :: E
+              REAL(KIND=8) :: OUT
+            END SUBROUTINE ETABLE
+          END INTERFACE 
+        END MODULE ETABLE__genmod
---
+++ MC3D_Structured_Initial/obj/Debug/output__genmod.f90
@@ -0,0 +1,7 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Apr 16 01:14:43 2014
+        MODULE OUTPUT__genmod
+          INTERFACE 
+            SUBROUTINE OUTPUT
+            END SUBROUTINE OUTPUT
+          END INTERFACE 
+        END MODULE OUTPUT__genmod
